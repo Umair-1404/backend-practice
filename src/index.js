45ff9f63a6a99ec2699e10fb485cceb6b95de306
Umair <saboowalaumair1404@gmail.com>
2025-04-22 13:12:11 +0530

    added utils code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import connectDB from "./db/index.js";
 import "dotenv/config";
+import app from "./app.js";
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server is listening on Port ${PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("MONGODB Connection Failed -----", err);
+  });
